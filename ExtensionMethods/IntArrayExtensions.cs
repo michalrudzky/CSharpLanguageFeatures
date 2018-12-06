f7b60b9027939d06c3a8cb987afd0429498772ac
USER\Michał <michal.rudzky@gmail.com>
2018-12-06 22:51:37 +0100

    Add example for Extension methods

---
+++ ExtensionMethods/IntArrayExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class IntArrayExtensions
+    {
+        public static int GetMaxValue(this int[] array)
+        {
+            int max = Int32.MinValue;
+
+            foreach (int number in array)
+            {
+                max = number > max ? number : max;
+            }
+
+            return max;
+        }
+
+        public static int GetMinValue(this int[] array)
+        {
+            int min = Int32.MaxValue;
+
+            foreach (int number in array)
+            {
+                if (number < min)
+                    min = number;
+            }
+
+            return min;
+        }
+
+        public static string Display(this int[] array)
+        {
+            string text = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                text += $"{array[i]}";
+
+                if (i == array.Length - 1)
+                    break;
+
+                text += ", ";
+            }
+
+            return text;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] {4, 16, 21, 7, 13};
+            int biggestNumber = numbers.GetMaxValue();
+            int smallestNumber = numbers.GetMinValue();
+
+            Console.WriteLine($"Numbers: {numbers.Display()}");
+            Console.WriteLine($"Biggest number: {biggestNumber}");
+            Console.WriteLine($"Smallest number: {smallestNumber}");
+        }
+    }
+}
