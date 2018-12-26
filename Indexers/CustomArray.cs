931f5f5c4be0e6819b07b7fce2da8e3ef0e875a4
USER\Michał <michal.rudzky@gmail.com>
2018-12-26 14:40:13 +0100

    Add Indexers example

---
+++ Indexers/CustomArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class CustomArray<T>
+    {
+        private T[] array;
+
+        public CustomArray(int size)
+        {
+            array = new T[size];
+        }
+
+        public int Length => array.Length;
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = -1;
+            do
+            {
+                Console.WriteLine("Enter the size of the array: ");
+                int.TryParse(Console.ReadLine(), out size);
+            } while (size <= 0);
+
+            var nums = new CustomArray<int>(size);
+            Random generator = new Random();
+            
+            // Fill the array with random numbers
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = generator.Next(1, 100);
+            }
+
+            Console.WriteLine("\nArray contents:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
